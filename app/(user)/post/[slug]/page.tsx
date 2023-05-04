f618754cb9a83762a5b4b15356a277088c1ab791
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-04 00:01:22 -0400

    portable text debugging

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -5,6 +5,7 @@ import urlFor from "../../../../lib/urlFor";
 import { PortableText } from "@portabletext/react";
 import { RichTextComponents } from "./RichTextComponents";
 
+
 type Props = {
   params: {
     slug: string;
@@ -39,7 +40,7 @@ async function Post({ params: { slug } }: Props) {
 
   return (
     <article className="px-10 pb-28">
-      <section className="space-y-2 border border-[#F7AB0A] text-white">
+      <section className="space-y-2 border-2 border-[#f70a0a] text-white">
         <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
           <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
             <Image
@@ -50,10 +51,10 @@ async function Post({ params: { slug } }: Props) {
             />
           </div>
 
-          <section className="p-5 bg-[#F7AB0A] w-full">
+          <section className="p-5 bg-[#504343dd] w-full">
             <div className="flex flex-col md:flex-row justify-between gap-y-5">
               <div>
-                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+                <h1 className="text-2xl font-extrabold">{post.title}</h1>
                 <p>
                   {new Date(post._createdAt).toLocaleDateString("en-US", {
                     day: "numeric",
@@ -67,8 +68,8 @@ async function Post({ params: { slug } }: Props) {
                   className="rounded-full"
                   src={urlFor(post.author.image).url()}
                   alt={post.author.name}
-                  height={40}
-                  width={40}
+                  height={60}
+                  width={60}
                 />
 
                 <div className="w-64">
